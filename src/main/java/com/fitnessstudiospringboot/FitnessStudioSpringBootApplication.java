5e1a57f24e1a390bfbf731a399c89f8058b0dfa5
hanovakova <hannanovakovva@gmail.com>
2025-09-13 17:09:58 +0200

    [main] Initialized project.

---
+++ src/main/java/com/fitnessstudiospringboot/FitnessStudioSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.fitnessstudiospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessStudioSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitnessStudioSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fitnessstudiospringboot/FitnessStudioSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitnessstudiospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessStudioSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
